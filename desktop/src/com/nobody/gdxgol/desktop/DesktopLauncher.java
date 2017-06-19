44fbf5f9ce08c9a5f0b0bf78399384faef757187
0xFireball <0xFireball@outlook.com>
2017-06-19 10:59:10 -0700

    create project

---
+++ desktop/src/com/nobody/gdxgol/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nobody.gdxgol.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nobody.gdxgol.GdxGol;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGol(), config);
+	}
+}
